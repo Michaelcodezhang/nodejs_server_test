154a032cd622915f0f182173a21260080582690c
张景浩 <zhangjinghao@hrsoft.net>
2017-12-10 16:26:58 +0800

    nodejs项目+静态页面

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+
+let root = path.resolve(process.argv[2] || './dist')
+
+console.log('Static root dir:'+root)
+
+let server = http.createServer(function (request,response) {
+  let pathname = url.parse(request.url).pathname
+  let filepath = path.join(root,pathname)
+  console.log(root)
+  console.log(filepath)
+  fs.stat(filepath,function (err,stats) {
+    if(!err&&stats.isFile()){
+      console.log('200'+request.url)
+      response.writeHead(200)
+      fs.createReadStream(filepath).pipe(response)
+    }else {
+      console.log('404'+request.url)
+      response.writeHead(404)
+      response.end('404 Not Found')
+    }
+  })
+})
+
+server.listen(8080)
+
+console.log('Server is running at http://127.0.0.1:8080/')
